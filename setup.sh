decc37005ca53b83e917f7599387b74aa2aea6b5
hbnguyennn <hoang.nguyen.rvc@gmail.com>
2023-04-28 17:01:32 +0700

    update

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+c_time=`date +"%Y_%m_%d_%H%M%S"`
+
+echo $c_time
+bk_place=${HOME}/dotfiles_bk${c_time}
+mkdir -p ${bk_place}
+
+for word in vimrc tmux.conf ctags  
+do
+   echo $word
+   if [ -f ${HOME}/.${word} ]; then
+      mv -v ${HOME}/.${word} ${bk_place}
+   fi
+   cp -v ${word} ${HOME}/.${word}
+done
+
+
